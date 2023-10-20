8e90086c845c928b3f11f8f1b0a41cb62a302d16
Maria Eduarda R. Gabriel <142342951+MariaEduardaRibeiro0212@users.noreply.github.com>
2023-10-19 21:20:19 -0300

    Create imprimirNumero.java
    Printar uma solicitação de entrada e depois ler e imprimir o número que o usuário digitou.

---
+++ imprimirNumero.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Atividade1 {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+
+        System.out.println("Digite um número:");
+        int num = scanner.nextInt();
+
+        System.out.println("O número informado foi " + num);
+
+
+    }
+}
